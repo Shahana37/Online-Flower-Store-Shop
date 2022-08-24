0098d6550006c610e4b370db276b5185cdc383b9
ShahanaParvin <109421243+Shahana37@users.noreply.github.com>
2022-08-24 21:40:30 +0600

    Update config.php

--- CSE470_Project/MVC Structure/Controller/config.php
+++ CSE470_Project/MVC Structure/Controller/config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$conn = mysqli_connect('localhost','root','','470 project') or die('connection failed');
 
 ?>
